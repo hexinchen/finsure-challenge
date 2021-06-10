84e4e43233b9550a85893d51ec98221be0735404
hex <chenhexin612@gmail.com>
2021-06-11 01:19:04 +1000

    finished edit functionality

--- myCtrl.js
+++ myCtrl.js
@@ -2,37 +2,56 @@ app.controller("myCtrl", function ($scope, $http, $mdDialog) {
     $scope.lenders = [];
     $scope.hasError = false;
     $scope.errorMsg = '';
+    $scope.isLoading = false;
 
-    $scope.isLoading = true;
+    $scope.loadLenderData = function () {
+        $scope.isLoading = true;
+        $http.get("lenders.json").then(response => {
+            $scope.isLoading = false;
+            if (response.status === 200 && response.data && response.data.data) {
+                $scope.lenders = response.data.data;
+            }
+        }, error => {
+            $scope.isLoading = false;
+            $scope.hasError = true;
+            $scope.errorMsg = error;
+        });
+    }
 
-    $scope.showPrompt = function (ev) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        var confirm = $mdDialog.prompt()
-            .title('What would you name your dog?')
-            .textContent('Bowser is a common name.')
-            .placeholder('Dog name')
-            .ariaLabel('Dog name')
-            .initialValue('Buddy')
-            .targetEvent(ev)
-            .required(true)
-            .ok('Okay!')
-            .cancel('I\'m a cat person');
-
-        $mdDialog.show(confirm).then(function (result) {
-            $scope.status = 'You decided to name your dog ' + result + '.';
-        }, function () {
-            $scope.status = 'You didn\'t name your dog.';
+    $scope.showEditDialog = function (lender) {
+        $mdDialog.show({
+            locals: { data: lender },
+            controller: DialogController,
+            templateUrl: 'edit-lender-dialog.html',
+            parent: angular.element(document.body),
+            targetEvent: lender,
+            clickOutsideToClose: true,
+            fullscreen: false
+        }).then(function (lender) {
+            $scope.isLoading = true;
+            var targetLenderIndex = $scope.lenders.findIndex(l => l.id === lender.id);
+            $scope.lenders[targetLenderIndex] = lender;
+            $scope.isLoading = false;
         });
     };
 
-    $http.get("lenders.json").then(response => {
-        $scope.isLoading = false;
-        if (response.status === 200 && response.data && response.data.data) {
-            $scope.lenders = response.data.data.map(data => data.attributes);
-        }
-    }, error => {
-        $scope.isLoading = false;
-        $scope.hasError = true;
-        $scope.errorMsg = error;
-    });
+    function DialogController($scope, $mdDialog, data) {
+        $scope.lender = angular.copy(data);
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+
+        $scope.onSave = function () {
+            $mdDialog.hide($scope.lender);
+        };
+
+    }
+
+    $scope.loadLenderData();
+
+
 });
